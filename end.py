d3344051523f7ea9e4b7a7023239d58bdbdfa10e
Scott <bronkscottema@gmail.com>
2022-03-21 15:38:39 -0400

    Lots of good work here.

--- end.py
+++ end.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-from PyQt5.QtCore import Qt, QSortFilterProxyModel
-from PyQt5.QtGui import QFont, QPixmap, QIcon, QFontDatabase, QStandardItemModel, QStandardItem
+from PyQt5.QtCore import Qt, QSortFilterProxyModel, QMimeData, QModelIndex
+from PyQt5.QtGui import QFont, QPixmap, QIcon, QFontDatabase, QStandardItemModel, QStandardItem, QDrag
 from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QFormLayout, QTableWidget, \
-    QHeaderView, QApplication, QTableWidgetItem, QTabWidget, QLineEdit, QTableView
+    QHeaderView, QApplication, QTableWidgetItem, QTabWidget, QLineEdit, QTableView, QAbstractItemView
 from psycopg2 import connect
 import psycopg2.extras
 from typing import Iterator, Dict, Any
@@ -19,7 +19,8 @@ conn = connect(dbname=os.getenv('DATABASE_NAME'), host=os.getenv('DATABASE_HOST'
 conn.autocommit = True
 cur = conn.cursor()
 
-roster_labels = [ "Player Id", "First Name", "Last Name", "Number", "Position"]
+recent_roster_keys = ["playerid", "last_name", "first_name", "jersey", "position", "height", "weight", "year" ]
+roster_labels = [ "Player Id", "Last Name", "First Name", "Number", "Position", "Height", "Weight", "Year"]
 roster_keys = [ "last_name", "first_name", "jersey", "position", "height", "weight", "year"]
 team_roster = [ "Last Name", "First Name", "Number", "Position", "Height", "Weight", "Year"]
 
@@ -32,9 +33,9 @@ class Window(QWidget):
         self.tab2 = QWidget()
         self.tab1 = QWidget()
         self.tabs = QTabWidget()
-        self.recently_viewed_table = QTableWidget(0, len(roster_labels))
+        self.recently_viewed_table = roster_recent()
         self.away_roster_table = QTableView()
-        self.home_roster_table = QTableView()
+        self.home_roster_table = home_table()
         self.setWindowIcon(QIcon('images/favicon.ico'))
         self.setGeometry(0, 0, 1920, 1080)
         self.setWindowTitle("Audible Analytics")
@@ -49,7 +50,7 @@ class Window(QWidget):
         main_layout = QHBoxLayout()
         field_layout = QVBoxLayout()
         roster_layout = QVBoxLayout()
-        image_layout = QVBoxLayout()
+        self.image_layout = QVBoxLayout()
 
         self.fieldpic.setPixmap(QPixmap('dottedfield.jpg'))
         field_layout.addWidget(self.fieldpic)
@@ -58,13 +59,18 @@ class Window(QWidget):
         pixmap = QPixmap('boxes.jpg')
         pixmap = pixmap.scaled(720, 480)
         self.play_pic.setPixmap(pixmap)
-        image_layout.addWidget(self.play_pic)
+        self.image_layout.addWidget(self.play_pic)
 
+        self.recently_viewed_model = QStandardItemModel(len(roster_labels), 1)
+        self.recently_filter_proxy_model = QSortFilterProxyModel()
+        self.recently_filter_proxy_model.setSourceModel(self.recently_viewed_model)
+        self.recently_filter_proxy_model.setFilterKeyColumn(0)
         header = self.recently_viewed_table.horizontalHeader()
+        self.recently_viewed_table.setModel(self.recently_filter_proxy_model)
         self.recently_viewed_table.horizontalHeader().setDefaultAlignment(Qt.AlignCenter)
         self.recently_viewed_table.horizontalHeader().setFont(self.table_font)
-        # self.api_table.clicked.connect(self.set_submit_button)
-        image_layout.addWidget(self.recently_viewed_table)
+        self.recently_viewed_table.verticalHeader().setVisible(False)
+        self.image_layout.addWidget(self.recently_viewed_table)
 
         #api roster data
         cur.execute(
@@ -87,7 +93,8 @@ class Window(QWidget):
         self.home_roster_table.setModel(self.filter_proxy_model)
         self.home_roster_table.horizontalHeader().setDefaultAlignment(Qt.AlignCenter)
         self.home_roster_table.horizontalHeader().setFont(self.table_font)
-        # self.api_table.clicked.connect(self.set_submit_button)
+        self.home_roster_table.verticalHeader().setVisible(False)
+
         self.tab1.layout.addWidget(self.search_field)
         self.tab1.layout.addWidget(self.home_roster_table)
         self.tab1.setLayout(self.tab1.layout)
@@ -103,17 +110,18 @@ class Window(QWidget):
         self.away_roster_table.setModel(self.away_filter_proxy_model)
         self.away_roster_table.horizontalHeader().setDefaultAlignment(Qt.AlignCenter)
         self.away_roster_table.horizontalHeader().setFont(self.table_font)
-        # self.api_table.clicked.connect(self.set_submit_button)
+        self.away_roster_table.verticalHeader().setVisible(False)
         self.tab2.layout.addWidget(self.away_search_field)
         self.tab2.layout.addWidget(self.away_roster_table)
         self.tab2.setLayout(self.tab2.layout)
 
         roster_layout.addWidget(self.tabs)
-        main_layout.addLayout(image_layout)
+        main_layout.addLayout(self.image_layout)
         main_layout.addLayout(roster_layout)
         self.setLayout(main_layout)
 
         self.show()
+
 
     def update_play_table(self):
         self.recently_viewed_table.setSortingEnabled(False)
@@ -143,66 +151,55 @@ class Window(QWidget):
                         " ",
                         0,
                         person[3],
+                        0,
+                        0,
+                        0,
                     ) for person in roster))
 
             insert_execute_values_iterator(conn, roster=play_table_result)
 
             try:
-                self.recently_viewed_table.setHorizontalHeaderLabels(roster_labels)
+                self.recently_viewed_model.setHorizontalHeaderLabels(roster_labels)
                 for n, key in enumerate(play_table_result):
-                    self.recently_viewed_table.insertRow(n)
-                    for column_number, data in enumerate(roster_labels):
-                        if data == "First Name" or data == "Last Name":
-                            item = QTableWidgetItem(str(" "))
-                            item.setTextAlignment(4)
-                            item.setFont(self.table_font)
-                            self.roster_table.setItem(n, column_number, item)
-                        elif data == "Player Id":
-                            item = QTableWidgetItem(str(key[0]))
-                            item.setTextAlignment(4)
-                            item.setFont(self.table_font)
-                            self.recently_viewed_table.setItem(n, column_number, item)
-                        elif data == "Number":
-                            item = QTableWidgetItem(str(0))
-                            item.setTextAlignment(4)
-                            item.setFont(self.table_font)
-                            self.recently_viewed_table.setItem(n, column_number, item)
-                        elif data == "Position":
-                            item = QTableWidgetItem(str(key[3]))
-                            item.setTextAlignment(4)
-                            item.setFont(self.table_font)
-                            self.recently_viewed_table.setItem(n, column_number, item)
+                    self.recently_viewed_model.insertRow(n)
+                    for column_number, data in enumerate(recent_roster_keys):
+                        if data == "first_name" or data == "last_name":
+                            item = QStandardItem(str(" "))
+                            self.recently_viewed_model.setItem(n, column_number, item)
+                        elif data == "playerid":
+                            item = QStandardItem(str(key[2]))
+                            self.recently_viewed_model.setItem(n, column_number, item)
+                        elif data == "jersey" or data == "weight" or data == "year" or data == "height":
+                            item = QStandardItem(str(0))
+                            self.recently_viewed_model.setItem(n, column_number, item)
+                        elif data == "position":
+                            item = QStandardItem(str(key[3]))
+                            self.recently_viewed_model.setItem(n, column_number, item)
                         else:
                             break
             except:
                 pass
 
         else:
-            cur.execute("select playerid, first_name, last_name, position from roster where playid = '" + result[0] + "' order by playerid;")
+            cur.execute("select playerid, last_name, first_name, position, height, weight, year from roster where playid = '" + result[0] + "' order by playerid;")
             play_table_result = cur.fetchall()
 
             try:
-                self.recently_viewed_table.setHorizontalHeaderLabels(roster_labels)
+                self.recently_viewed_model.setHorizontalHeaderLabels(roster_labels)
                 for n, key in enumerate(play_table_result):
-                    self.recently_viewed_table.insertRow(n)
+                    self.recently_viewed_model.insertRow(n)
                     for column_number, data in enumerate(roster_labels):
                         if data == "First Name" or data == "Last Name":
                             continue
-                        elif data == "Number":
-                            item = QTableWidgetItem(str(0))
-                            item.setTextAlignment(4)
-                            item.setFont(self.table_font)
-                            self.recently_viewed_table.setItem(n, column_number, item)
+                        elif data == "Number" or data == "Height" or data == "Weight" or data == "Year":
+                            item = QStandardItem(str(0))
+                            self.recently_viewed_model.setItem(n, column_number, item)
                         elif data == "Position":
-                            item = QTableWidgetItem(str(key[3]))
-                            item.setTextAlignment(4)
-                            item.setFont(self.table_font)
-                            self.recently_viewed_table.setItem(n, column_number, item)
+                            item = QStandardItem(str(key[3]))
+                            self.recently_viewed_model.setItem(n, column_number, item)
                         else:
-                            item = QTableWidgetItem(str(key[column_number]))
-                            item.setTextAlignment(4)
-                            item.setFont(self.table_font)
-                            self.recently_viewed_table.setItem(n, column_number, item)
+                            item = QStandardItem(str(key[column_number]))
+                            self.recently_viewed_model.setItem(n, column_number, item)
             except:
                 pass
 
@@ -241,6 +238,64 @@ class Window(QWidget):
         else:
             print("NFL")
 
+
+class roster_recent(QTableView):
+
+    def __init__(self):
+        super().__init__()
+        self.setAcceptDrops(True)
+        # self.setDragEnabled(True)
+        self.setDragDropOverwriteMode(True)
+        # self.setDragDropMode(QAbstractItemView.DropOnly)
+        # self.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+    def dragEnterEvent(self, e):
+        e.accept()
+
+    def dropEvent(self, event):
+        if self.viewport().rect().contains(event.pos()):
+            fake_model = QStandardItemModel()
+            fake_model.dropMimeData(
+                event.mimeData(), event.dropAction(), 0, 0, QModelIndex()
+            )
+            print("from:")
+            for r in range(fake_model.rowCount()):
+                for c in range(fake_model.columnCount()):
+                    ix = fake_model.index(r, c)
+                    print(ix.data())
+            to_index = self.indexAt(event.globalPos())
+            if to_index.isValid():
+                print("to:", to_index.data())
+        super().dropEvent(event)
+
+
+    def dragMoveEvent(self, e):
+        e.accept()
+
+class home_table(QTableView):
+
+    def __init__(self):
+        super(home_table, self).__init__()
+        self.setDragEnabled(True)
+        self.setDropIndicatorShown(True)
+        self.setDragDropMode(QAbstractItemView.DragDrop)
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+
+    def dragEnterEvent(self, e):
+        e.accept()
+
+    def dropEvent(self, e):
+
+        position = e.pos()
+        QStandardItem.move(position)
+
+        e.setDropAction(Qt.MoveAction)
+
+        e.accept()
+
+    def dragMoveEvent(self, e):
+        e.accept()
 
 
 def end_page():
